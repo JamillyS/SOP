b3f4fc71a149e8e136c35f27e004c9c3a70d7861
Jamilly da Silva Pinheiro <jamilly.p11@aluno.ifsc.edu.br>
2024-04-25 17:48:58 -0300

    Fazendo o desafio 2

--- LABS/LAB-6/estacionamento2.c
+++ LABS/LAB-6/estacionamento2.c
@@ -7,91 +7,101 @@
 #define NUM_CARROS 20
 #define NUM_STEPS 10
 
-sem_t s;
-int num = 0;
-pthread_mutex_t mutex_num;
-pthread_mutex_t Cancela[7];
+sem_t s;                    // Semáforo
+int num = 0;                // num é o número de carros
+pthread_mutex_t mutex_num;  // mutex de num
+pthread_mutex_t Cancela[7]; // um mutex para cada cancela
 
-int fila[7] = {0};
+pthread_mutex_t mutex_fila; // para poder imprimir a fila de carros
 
+int fila[NUM_CARROS];       // fila de carros
 
-typedef struct {
-    int n_carro;     // o id é o numero do carro
+struct carro {
+    int n_carro;         // id do carro
     int cancela;
-} Carro;
+};
 
-void *carroEstacionando(void *arg) {
+void *carroEstacionando(void *){
 
-    Carro *carro = (Carro *) arg;
-
-    pthread_mutex_lock(&mutex_num);
-    num++;
+    pthread_mutex_lock(&mutex_num);     //bloqueia o mutex_num
+    num++;                              //incrementa o num
     int meu_num = num;
-    pthread_mutex_unlock(&mutex_num);
+    pthread_mutex_unlock(&mutex_num);   //desbloqueia o mutex_num
 
-   printf("Carro %d chegou na cancela %d ", meu_num, carro->cancela);
-   printf(" Fila: [");
-
-    for (int i = 0; i < 7; i++) {
-        if (fila[i] != 0) {
-            printf("%d, ", fila[i]);
-        }  
-    }
-    printf("]\n");
-
-    fila[carro->cancela] = meu_num;
-    pthread_mutex_unlock(&Cancela[carro->cancela]);
+    int cancela = rand() % 7;
 
     sem_wait(&s);
+    printf("Carro %d chegou na cancela %d\n", meu_num, cancela);
 
-    pthread_mutex_lock(&Cancela[carro->cancela]);
-    printf("Carro %d entrando pela cancela %d\n", meu_num, carro->cancela);
-    sleep(5);
-    //pthread_mutex_unlock(&mute_num);
-    pthread_mutex_unlock(&Cancela[carro->cancela]);
+    pthread_mutex_lock(&mutex_fila);
+    fila[meu_num] = cancela;
+    pthread_mutex_unlock(&mutex_fila);
+
+    pthread_mutex_lock(&mutex_num);     //bloqueia o mutex_num
+
+    printf("[");
+    for (int i = 1; i <= num; i++){
+        if(fila[i] == cancela){
+            printf(" %d,", i);
+        }
+    }
+    printf(" ]");
+
+    printf("Carro %d entrando pela cancela %d\n", meu_num, cancela);
+    sleep(5);                           //Atraso, para simular a passagem do carro pela cancela
+    //pthread_mutex_unlock(&Cancela[cancela]);
+    pthread_mutex_unlock(&mutex_num);   //desbloqueia o mutex_num
 
     int sorteio = rand() % 10;
-    sleep(10 + sorteio);
-    sem_post(&s);
+    sleep(10 + sorteio);            //Atraso, para simular o tempo que o carro fica no estacionamento
 
-    pthread_mutex_lock(&Cancela[carro->cancela]);
-    fila[carro->cancela] = 0;
-    pthread_mutex_unlock(&Cancela[carro->cancela]);
-
-
+    sem_post(&s);                   //Libera a vaga no estacionamento
     printf("Carro %d saindo do estacionamento\n", meu_num);
+
+    pthread_mutex_lock(&mutex_fila);
+    
+
     pthread_exit(NULL);
 
 }
 
-int main(int argc, char *argv[]) {
-    pthread_t carro[NUM_CARROS];
+int main(int argc, char *argv[]){
+
+    pthread_t carro[NUM_CARROS];        //threads
     pthread_attr_t attr;
-    long status;
+    long status;                        //status da thread
+
     int i;
 
-    sem_init(&s, 0, NUM_STEPS);
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    sem_init(&s, 0, NUM_STEPS);             //inicializa o semáforo
+    pthread_mutex_init(&mutex_num, NULL);   //inicializa o mutex_num
+    pthread_mutex_init(&mutex_fila, NULL);  //inicializa o mutex_fila
+    // for(i = 0; i < 7; i++){
+    //     pthread_mutex_init(&Cancela[i], NULL);
+    // }
 
-    for (i = 0; i < NUM_CARROS; i++) {
-        Carro *car = (Carro *) malloc(sizeof(Carro));
-        car->n_carro = i;
-        car->cancela = rand() % 7;
-        status = pthread_create(&carro[i], &attr, carroEstacionando, (void *) car);
-        if (status) {
-            perror("pthread_create");
-            exit(1);
+    pthread_attr_init(&attr);           //inicializa a thread
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); //estado da thread
+
+    for(i = 0; i < NUM_CARROS; i++){
+        //Talvez no final seja um NULL
+        status = pthread_create(&carro[i], &attr, carroEstacionando, NULL);
+        if(status){
+            perror("pthread_create") ;
+            exit (1);
         }
     }
 
-    for (i = 0; i < NUM_CARROS; i++) {
-        pthread_join(carro[i], NULL);
+    for(i = 0; i < NUM_CARROS; i++){
+        status = pthread_join (carro[i], NULL);
+        if(status){
+            perror ("pthread_join") ;
+            exit (1) ;
+        }
     }
 
-    pthread_attr_destroy(&attr);
+    pthread_attr_destroy (&attr) ;
     pthread_mutex_destroy(&mutex_num);
-    sem_destroy(&s);
-    pthread_exit(NULL);
-    return 0;
+
+
 }
