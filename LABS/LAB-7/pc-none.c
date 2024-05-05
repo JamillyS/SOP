477181c6772be1d2a229ead553df06cb3df97878
Jamilly da Silva Pinheiro <jamilly.p11@aluno.ifsc.edu.br>
2024-05-05 15:27:08 -0300

    Laboratório 7 terminado

--- LABS/LAB-7/pc-none.c
+++ LABS/LAB-7/pc-none.c
@@ -7,10 +7,15 @@ Compilar com gcc -Wall pc-none.c -o pc-none -lpthread
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define NUM_PROD 3
 #define NUM_CONS 5
 #define VAGAS 6
+
+sem_t mutex;
+sem_t item;
+sem_t vaga;
 
 int num_itens, num_vagas ; 	// contadores de vagas e peças
 
@@ -22,48 +27,62 @@ void espera (int n)
 }
 
 // corpo de produtor
-void *prodBody (void *id)
-{
+void *prodBody (void *id){
+
   long tid = (long) id ;
 
   printf ("P%02ld: Olá!\n", tid) ;
 
   while (1)
   {
+    sem_wait(&vaga);
+    sem_wait(&mutex);
     num_itens++ ;
     num_vagas-- ;
     printf ("P%02ld: put an item (%d itens, %d vagas)!\n",
             tid, num_itens, num_vagas) ;
+    sem_post(&mutex);
+    sem_post(&item);
     espera (2) ;
+
   }
+
 }
 
 // corpo de consumidor
-void *consBody (void *id)
-{
+void *consBody (void *id){
+
   long tid = (long) id ;
 
   printf ("C%02ld: Olá!\n", tid) ;
 
   while (1)
   {
+    sem_wait(&item);
+    sem_wait(&mutex);
     num_itens-- ;
     num_vagas++ ;
     printf ("C%02ld: got an item (%d itens, %d vagas)!\n",
             tid, num_itens, num_vagas) ;
+    sem_post(&mutex);
+    sem_post(&vaga);
     espera (2) ;
-  }
+    }
+
 }
 
-// programa principal
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]){
+
   pthread_t produtor   [NUM_PROD] ;
   pthread_t consumidor [NUM_CONS] ;
   long i ;
 
   num_itens = 0 ;
   num_vagas = VAGAS ;
+
+  sem_init(&mutex, 0, 1);
+  sem_init(&item, 0, 0);
+  sem_init(&vaga, 0, VAGAS);
 
   // cria produtores
   for (i=0; i<NUM_PROD; i++)
--- LABS/LAB-7/rw-none.c
+++ LABS/LAB-7/rw-none.c
@@ -7,10 +7,12 @@ Compilar com gcc -Wall rw-none.c -o rw-none -lpthread
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define NUM_READERS 5
 #define NUM_WRITERS 2
 
+sem_t mutex ;		
 int shared ;		// área compartilhada
 
 // espera um tempo aleatório entre 0 e n segundos (float)
@@ -21,37 +23,41 @@ void espera (int n)
 }
 
 // corpo das tarefas leitoras
-void *readerBody (void *id)
-{
-  long tid = (long) id ;
+void *readerBody (void *id){
 
+  long tid = (long) id ;
   while (1)
   {
-    // faz a leitura
+    sem_wait(&mutex);
     printf ("R%02ld: read %d\n", tid, shared) ;
+    sem_post(&mutex);
     espera (2) ;
 
     printf ("R%02ld: out\n", tid) ;
     espera (2) ;
   }
+
 }
 
-// corpo das tarefas escritoras
-void *writerBody (void *id)
-{
-  long tid = (long) id ;
 
+// corpo das tarefas escritoras
+void *writerBody (void *id){
+  //com semaforo
+  long tid = (long) id ;
   while (1)
   {
-    // faz a escrita
+    sem_wait(&mutex);
     shared = random () % 1000 ;
     printf ("\t\t\tW%02ld: wrote %d\n", tid, shared) ;
+    sem_post(&mutex);
     espera (2) ;
 
     printf ("\t\t\tW%02ld: out\n", tid) ;
     espera (2) ;
   }
+
 }
+
 
 int main (int argc, char *argv[])
 {
@@ -60,6 +66,8 @@ int main (int argc, char *argv[])
   long i ;
 
   shared  = 0 ;
+
+  sem_init(&mutex, 0, 1);
 
   // cria leitores
   for (i=0; i<NUM_READERS; i++)
